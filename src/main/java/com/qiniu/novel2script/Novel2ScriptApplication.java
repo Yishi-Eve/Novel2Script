6384390675298f68fea83b3c777a64d803baf1ce
YishiEve <578073465@qq.com>
2026-06-05 15:22:03 +0800

    Initial commit

---
+++ src/main/java/com/qiniu/novel2script/Novel2ScriptApplication.java
@@ -0,0 +1,13 @@
+package com.qiniu.novel2script;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Novel2ScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Novel2ScriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qiniu/novel2script/Novel2ScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiniu.novel2script;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Novel2ScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
